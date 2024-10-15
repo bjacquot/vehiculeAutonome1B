0a9b70da983f45e3d8063ac0d2a8b272493a2bf5
SAMAHRI MOHAMED JIBRIL <sama0019@028F2-0F0002003.local.iut-troyes.univ-reims.fr>
2024-10-15 12:15:09 +0200

    classe controleur

---
+++ controleur.cpp
@@ -0,0 +1,51 @@
+#include "controleur.h"
+#include <QDebug>
+
+Controleur::Controleur(QObject *parent, array<int, 360>& distances)
+    : QObject(parent), distances_mm(distances), erreur_precedente(0), somme_erreurs(0)
+{
+
+}
+
+void Controleur::newDatas()
+{
+    // Initialisation:
+    double vitesse_max = 1;
+    double vitesse = 0.1;
+    int angle_cible = 30;
+    int distance_cible = 600;
+
+    // correction de l'angle:
+    int index_cible = (angle_cible + 180) % 360;
+    double distance_actuelle = distances_mm[index_cible];
+    double erreur = (distance_cible/600) - (distance_actuelle/600);
+    somme_erreurs += erreur;
+    double variation_erreur = erreur - erreur_precedente;
+    double commande_a = kp * erreur + ki * somme_erreurs + kd * variation_erreur;
+    erreur_precedente = erreur;
+    if (commande_a > 1) commande_a = 1;
+    if (commande_a < -1) commande_a -1;
+
+    // correction de la vitesse :
+    if (distance_actuelle <= distance_cible) {
+        vitesse = vitesse_max * ((distance_actuelle) / distance_cible)+0.05;
+    } else {
+        vitesse = 0.1;
+    }
+    if (vitesse > vitesse_max) vitesse = vitesse_max;
+    if (vitesse < -vitesse_max) vitesse = -vitesse_max;
+
+    // emition du signal + affichage des valeurs
+    emit deplacer(vitesse, commande_a);
+    qDebug() << "Correction:" << commande_a;
+    qDebug() << "vitesse:" << vitesse;
+
+
+}
+
+void Controleur::initPID(double _kp, double _ki, double _kd)
+{
+    kp = _kp;
+    ki = _ki;
+    kd = _kd;
+}
---
+++ controleur.h
@@ -0,0 +1,36 @@
+#ifndef CONTROLEUR_H
+#define CONTROLEUR_H
+
+#include <QObject>
+#include <array>
+
+using std::array;
+
+class Controleur : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Controleur(QObject *parent, array<int, 360>& distances);
+    // coefficients:
+    double kp;
+    double ki;
+    double kd;
+    void initPID(double _kp, double _ki, double _kd);
+    // erreurs:
+    double erreur_precedente;
+    double somme_erreurs;
+    // tableau de valeur
+    array<int, 360>& distances_mm;
+
+signals:
+    void deplacer(double vitesse, double commande_a);
+
+public slots:
+    void newDatas();
+
+private:
+
+};
+
+#endif
